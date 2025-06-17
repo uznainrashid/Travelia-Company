dbe527699a9ddbe1c947fc3924442cd9c9b1cf70
uznainrashid <147235060+uznainrashid@users.noreply.github.com>
2025-06-17 12:33:33 +0500

    Complete Projects

--- assets/js/travel.js
+++ assets/js/travel.js
@@ -4,28 +4,39 @@ window.addEventListener("load", () => {
 });
 
 // Add event on multiple element
-const addEventOnElements = function (elements, eventType, callback){
-    for(let i = 0 ; i < elements.length  ; i++){
-        elements[i].addEventListener(eventType , callback)
-    }
+// const addEventOnElements = function (elements, eventType, callback) {
+//   for (let i = 0; i < elements.length; i++) {
+//     elements[i].addEventListener(eventType, callback);
+//   }
+// };
+function addEventOnElements(elements, eventType, callback) {
+    elements.forEach(element => element.addEventListener(eventType, callback));
+    return elements; // This ensures it doesn't return undefined
 }
-
 
 // Navbar
 const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]")
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
-const Togglenav = function (){
-    navbar.classList.add("active")
-    overlay.classList.add("active")
-    document.body.classList.add("nav-active")
-}
-addEventOnElements(navTogglers , "click", Togglenav)
-console.log(addEventOnElements(navTogglers))
-
+const Togglenav = function () {
+  navbar.classList.add("active");
+  overlay.classList.add("active");
+  document.body.classList.add("nav-active");
+};
+addEventOnElements(navTogglers, "click", Togglenav);
+console.log(addEventOnElements(navTogglers));
 
 /*  # HEADER */
 const header = document.querySelector("[data-header]");
-window.addEventListener("scroll", function (){
-    header.classList[window.scrollY > 100 ? "add" : "remove"]("active")
-})
+window.addEventListener("scroll", function () {
+  header.classList[window.scrollY > 100 ? "add " : "remove"]("active");
+});
+// Hamburger close button
+document.querySelector(".nav-toggle-btn").addEventListener("click", () => {  
+  document.querySelector("[data-navbar]").classList.toggle("active");  
+});
+
+document.querySelector(".nav-toggle-btn").addEventListener("click", (e) => {
+  console.log(e);
+  e.views.closed = true;
+});
